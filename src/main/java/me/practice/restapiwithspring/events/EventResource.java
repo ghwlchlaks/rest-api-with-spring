5a15f39fc2a03f59ad09a32cd63bf6ea48950d69
ghwlchlaks <ghwlchlaks@naver.com>
2021-03-10 01:14:35 +0900

    응답에 hateoas 링크 추가  - self  (view)  - update (만든사람은 수정가능)  - events (목록으로가는 링크)

---
+++ src/main/java/me/practice/restapiwithspring/events/EventResource.java
@@ -0,0 +1,15 @@
+package me.practice.restapiwithspring.events;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+// bean serialze
+public class EventResource extends EntityModel<Event> {
+
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+    }
+}
